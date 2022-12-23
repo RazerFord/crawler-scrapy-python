7980f0991d65a31ab16d979e9b791b3628edb4a8
RazerFord <desk10567@gmail.com>
2022-12-23 12:41:04 +0300

    init database connection

--- netology/netology/pipelines.py
+++ netology/netology/pipelines.py
@@ -7,6 +7,8 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
+import psycopg2
+
 
 class NetologyPipeline:
     def __init__(self):
@@ -14,8 +16,32 @@ class NetologyPipeline:
 
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
-        if adapter['program_id'] in self.ids_seen:
+        if adapter["program_id"] in self.ids_seen:
             raise DropItem(f"Duplicate item found: {item!r}")
         else:
-            self.ids_seen.add(adapter['program_id'])
+            self.ids_seen.add(adapter["program_id"])
         return item
+
+class DatabasePipeline:
+    def open_spider(self, spider):
+        hostname = "db"
+        username = "postgres"
+        password = "postgres"
+        database = "netology"
+        try:
+            self.connection = psycopg2.connect(
+                host=hostname, user=username, password=password, dbname=database
+            )
+            self.cur = self.connection.cursor()
+            print("open database connection")
+        except Exception:
+            print("error database connection")
+
+    def close_spider(self, spider):
+        if self.cur is not None:
+            self.cur.close()
+            self.connection.close()
+            print("close database connection")
+
+    def process_item(self, item, spider):
+        pass
--- netology/netology/settings.py
+++ netology/netology/settings.py
@@ -64,6 +64,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "netology.pipelines.NetologyPipeline": 300,
+    "netology.pipelines.DatabasePipeline": 350
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
