26d61b3d7d6900fe26010bef06cd40dc1e39f9d9
RazerFord <desk10567@gmail.com>
2023-02-10 00:28:33 +0000

    add schedule

--- script.py
+++ script.py
@@ -1,27 +1,40 @@
 from os import environ
 import subprocess
 from time import sleep
+import schedule
 
+iteration = 1
+time = str()
 
 def run():
-    iteration = 1
-    while True:
-        with open("log", "a") as f:
-            print("start")
-            f.write(f"start scrapy iteration {iteration}\n")
-            out = subprocess.check_output(
-                "cd ./netology/ && scrapy crawl programs --logfile log",
-                shell=True,
-                universal_newlines=True,
-            )
-            f.write(f"time break: {environ['TIME_BREAK']}\n")
-            f.write(out)
-            f.write("stop scrapy\n\n")
-            iteration += 1
-            print("sleep")
-            sleep(int(environ["TIME_BREAK"]))
+    global iteration
+    with open("log", "a") as f:
+        print("start crawl")
+        f.write(f"start scrapy iteration {iteration}\n")
+        out = subprocess.check_output(
+            "cd ./netology/ && scrapy crawl programs --logfile log",
+            shell=True,
+            universal_newlines=True,
+        )
+        f.write(time)
+        f.write(out)
+        f.write("stop scrapy\n\n")
+        iteration += 1
+        print("sleep crawl")
 
+
+def initSchedule():
+    sleep(5)
+    global time
+    if "TIME_BREAK" in environ:
+        schedule.every(environ["TIME_BREAK"]).seconds.do(run)
+        time = f"time break: {environ['TIME_BREAK']}\n"
+    elif "EXACT_TIME" in environ:
+        schedule.every().day.at(environ["EXACT_TIME"]).do(run)
+        time = f"exact time: {environ['EXACT_TIME']}\n"
 
 if __name__ == "__main__":
-    sleep(10)
-    run()
+    initSchedule()
+    while True:
+        schedule.run_pending()
+        sleep(1)
